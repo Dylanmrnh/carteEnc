66b701727ab278c59072137764a3d7c7e7be0054
dmarinheiro <dyalnmarinheiro@gmail.com>
2022-03-22 12:30:21 +0100

    2

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::resource('demandeCarte', 'CarteEncController');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
